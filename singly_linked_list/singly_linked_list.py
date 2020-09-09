c02b667a678c4c48bd42fb8c44f8e4b5f979d810
Parimala Vemula <parimala.thodupunoori@gmail.com>
2020-09-09 00:05:33 -0700

    doubly_linked_list

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -58,3 +58,103 @@ class LinkedList:
             previous.next = None
 
         return rv
+
+# 👍 ANSWER 2
+
+
+class Node2:
+    def __init__(self, value=None, nextNode=None):
+        self.value = value
+        self.nextNode = nextNode
+
+    def get_value(self):
+        return self.value
+
+    def get_next(self):
+        return self.nextNode
+
+    def set_next(self, newNext):
+        self.nextNode = newNext
+
+
+class LinkedList2:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_to_tail(self, value):
+        # wrap the input value in a nodex
+        new_node = Node(value, None)
+        if self.head is None:  # list is empty
+            self.head = new_node
+            self.tail = new_node
+        else:  # list is non-empty
+            self.tail.set_next = new_node  # reference/pointer
+            self.tail = new_node
+
+    def remove_head(self):
+        if not self.head:  # list is empty
+            return None
+        elif not self.head.get_next():  # list has single element
+            head = self.head  # reference to the head
+            self.head = None  # delete the reference
+            self.tail = None  # tail isn't referring to anything
+            return head.get_value()
+        else:  # list has > 1 element
+            value = self.head.get_value()
+            self.head = self.head.get_next()  # head reference to current head's next node in the list
+            return value
+
+    def remove_tail(self):
+        if not self.head:  # list has 0 elements or empty
+            return None
+
+        if self.head is self.tail:  # list has 1 element
+            value = self.head.get_value()
+            self.head = None
+            self.tail = None
+            return value
+
+        current = self.head
+
+        while current.get_next() is not self.tail:  # list has more elems
+            current = current.get_next()
+
+        value = self.tail.get_value()
+        self.tail = current
+        return value
+
+
+    def contains(self, value):
+        if not self.head:
+            return False
+
+        # Recursive solution
+        def search(node):
+            if node.get_value() == value:
+                return True
+            if not node.get_next():
+                return False
+            return search(node.get_next())
+        return search(self.head)
+
+        # current node reference
+        current = self.head
+        while current:  # if current is valid or true
+            if current.get_value() == value:  # current value matches target value
+                return True
+            current = current.get_next()  # update current node to next node in the list
+        # target node isn't in the list
+        return False
+
+    def get_max(self):
+        if not self.head:
+            return None
+        max_value = self.head.get_value()
+        current = self.head.get_next()  # current node
+        while current: # valid list node 
+            if current.get_value > max_value:
+                max_value = current.get_value()
+                # update current node to next node in the list
+                current = current.get_next()
+        return max_value
--- stack/stack.py
+++ stack/stack.py
@@ -44,6 +44,28 @@ return elements in Last In First Out order.
 # print(len(myStack))
 # print(myStack)
 
+from singly_linked_list import LinkedList
+
+
+class Stack:
+    def __init__(self):
+        self.size = 0
+        self.storage = LinkedList()
+
+    def __len__(self):
+        return self.size
+        # return len(self.storage)
+
+    def push(self, value):
+        self.storage.add_to_tail(value)
+        self.size += 1
+
+    def pop(self):
+        if self.size == 0:
+            return None
+        self.size -= 1
+        self.storage.remove_tail()
+
 
 class Stack:
     def __init__(self):
